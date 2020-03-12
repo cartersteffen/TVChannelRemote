d8eaffbdcea6b95c01bba3302dc1aa6327c171e9
Carter Steffen <cartste3@gmail.com>
2020-03-11 23:57:19 -0500

    Initial commit

---
+++ app/src/main/java/com/example/tvchannelremote/ui/adapters/TVChannelsAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.tvchannelremote.ui.adapters
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class TVChannelsAdapter : RecyclerView.Adapter<TVChannelsAdapter.TVChannelViewHolder>() {
+
+
+    class TVChannelViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TVChannelViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: TVChannelViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
